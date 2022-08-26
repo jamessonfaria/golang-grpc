887776b944f578696e1a1136b8851ce43b1c1d65
Jamesson Faria <jamessonjr@gmail.com>
2022-08-26 18:27:09 -0300

    Implement server stream

--- services/user.go
+++ services/user.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"time"
 	"fmt"
 	"context"
 	"github.com/jamessonfaria/golang-grpc/pb"
@@ -23,4 +24,44 @@ func (*UserService) AddUser(ctx context.Context, req *pb.User) (*pb.User, error)
 		Name: req.GetName(),
 		Email: req.GetEmail(), 
 	}, nil
+}
+
+func (*UserService) AddUserVerbose(req *pb.User, stream pb.UserService_AddUserVerboseServer) error {
+	stream.Send(&pb.UserResultStream{
+		Status: "Init",
+		User: &pb.User{},
+	}) 
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Inserting",
+		User: &pb.User{},
+	}) 
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "User has been inserted",
+		User: &pb.User{
+			Id: "123",
+			Name: req.GetName(),
+			Email: req.GetEmail(),
+		},
+	}) 
+
+	time.Sleep(time.Second * 3)
+
+	stream.Send(&pb.UserResultStream{
+		Status: "Completed",
+		User: &pb.User{
+			Id: "123",
+			Name: req.GetName(),
+			Email: req.GetEmail(),
+		},
+	}) 
+
+	time.Sleep(time.Second * 3)
+
+	return nil	
 }
