8251382234ad6086cbaead24bdaf703b458dca55
Jamesson Faria <jamessonjr@gmail.com>
2022-08-26 18:11:05 -0300

    Implement client

---
+++ cmd/client/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/jamessonfaria/golang-grpc/pb"
+	"log"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	connection, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect to gRPC Server: %v", err)
+	}
+	defer connection.Close()
+
+	client := pb.NewUserServiceClient(connection)
+	AddUser(client)
+}
+
+func AddUser(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id: "0",
+		Name: "Joao",
+		Email: "j@j.com",
+	}
+
+	res, err := client.AddUser(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	fmt.Println(res)
+
+}
