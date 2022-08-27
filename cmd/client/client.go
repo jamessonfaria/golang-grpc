e3014f9fbc23ef6c410756af4ce79e07a31837d4
Jamesson Faria <jamessonjr@gmail.com>
2022-08-27 11:07:46 -0300

    Implement clientstreaming

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"time"
 	"io"
 	"context"
 	"fmt"
@@ -18,7 +19,8 @@ func main() {
 
 	client := pb.NewUserServiceClient(connection)
 	//AddUser(client)
-	AddUserVerbose(client)
+	//AddUserVerbose(client)
+	AddUsers(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -61,4 +63,46 @@ func AddUserVerbose(client pb.UserServiceClient) {
 
 		fmt.Println("Status: ", stream.Status, " - ", stream.GetUser())
 	}
+}
+
+func AddUsers(client pb.UserServiceClient) {
+	reqs := []*pb.User{
+		&pb.User{
+			Id: "w1",
+			Name: "James 1",
+			Email: "james1@aol.com",
+		},
+		&pb.User{
+			Id: "w2",
+			Name: "James 2",
+			Email: "james2@aol.com",
+		},
+		&pb.User{
+			Id: "w3",
+			Name: "James 3",
+			Email: "james3@aol.com",
+		},
+		&pb.User{
+			Id: "w4",
+			Name: "James 4",
+			Email: "james4@aol.com",
+		},
+	}
+
+	stream, err := client.AddUsers(context.Background())
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	for _, req := range reqs {
+		stream.Send(req)
+		time.Sleep(time.Second * 3)
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error receiving response: %v", err)
+	}
+	
+	fmt.Println(res)
 }
